5ea86695a067216562946a25799efaac7947ef12
ValeriiaPetryna <valeriapetrina1@gmail.com>
2024-04-04 22:01:44 +0300

    finish

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 const API_KEY = '43195893-e6aecd5c5261fd0c345764808';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-export function getData(q) {
+
+export function getData(q, page, per_page) {
   const params = new URLSearchParams({
     key: API_KEY,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    per_page,
+    page,
     q,
   });
   return axios.get('?' + params).then(response => response.data);
--- src/main.js
+++ src/main.js
@@ -2,49 +2,102 @@ import SimpleLightbox from 'simplelightbox';
 import iziToast from 'izitoast';
 import { getData } from './js/pixabay-api';
 import { createMarkup } from './js/render-functions';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const lightbox = new SimpleLightbox('.gallery a', {
-  /* options */
-});
+const lightbox = new SimpleLightbox('.gallery a', {});
+
+let page = 1;
+let message = '';
+const per_page = 15;
 
 const formEl = document.querySelector('.form');
 const galleryEl = document.querySelector('.gallery');
 let preloader = document.querySelector('.preloader');
+let btnEl = document.querySelector('.btn');
 
-formEl.addEventListener('submit', event => {
-  event.preventDefault();
-  const data = Object.fromEntries(new FormData(event.target));
+formEl.addEventListener('submit', async event => {
+  try {
+    event.preventDefault();
+    const data = Object.fromEntries(new FormData(event.target));
+    message = data.message;
+    if (message === '') {
+      return;
+    }
+    page = 1;
+    preloader.classList.add('show');
+    btnEl.classList.remove('show');
 
-  if (data.message === '') {
-    return;
-  }
-  preloader.classList.add('show');
-  getData(data.message)
-    .then(response => {
-      if (response.hits.length === 0) {
-        iziToast.info({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-        return;
-      }
-      const markup = createMarkup(response.hits);
-      console.log(markup);
-      addMarkup(markup);
-      lightbox.refresh();
-    })
-    .catch(err => {
-      iziToast.error({
-        message: 'Error!!!',
+    const { totalHits, hits } = await getData(message, page, per_page);
+
+    if (hits.length === 0) {
+      iziToast.info({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
       });
-    })
-    .finally(() => {
-      preloader.classList.remove('show');
+      return;
+    }
+
+    if (totalHits > per_page) {
+      btnEl.classList.add('show');
+    } else {
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+    }
+
+    const markup = createMarkup(hits);
+
+    addMarkup(markup);
+    lightbox.refresh();
+    scrollPage();
+  } catch (error) {
+    iziToast.error({
+      message: 'Error!!! ' + error.message,
     });
+  } finally {
+    preloader.classList.remove('show');
+  }
+});
+
+btnEl.addEventListener('click', async () => {
+  try {
+    page += 1;
+    const { totalHits, hits } = await getData(message, page, per_page);
+    const markup = createMarkup(hits);
+
+    if (page * hits.length >= totalHits) {
+      btnEl.classList.remove('show');
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+    }
+
+    updateMarkup(markup);
+    lightbox.refresh();
+    scrollPage();
+  } catch (error) {
+    iziToast.error({
+      message: 'Error!!! ' + error.message,
+    });
+  } finally {
+    preloader.classList.remove('show');
+  }
 });
 
 function addMarkup(markup) {
   galleryEl.innerHTML = markup;
+}
+
+function updateMarkup(markup) {
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+}
+
+function scrollPage() {
+  let { height } = galleryEl.children[0].getBoundingClientRect();
+  window.scrollBy({
+    top: height * 2,
+    behavior: 'smooth',
+  });
 }
